a750d624b364e47d24e2ed97bdab449dd6482e90
krnveersharma <115925693+krnveersharma@users.noreply.github.com>
2024-07-28 22:59:40 +0530

    Added api feature for apps integration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,29 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/','/api/webhook']);
+
+export function middleware(req: NextRequest, event: NextFetchEvent) {
+  let response = req.method === "OPTIONS" ? new NextResponse(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*", 
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  }) : NextResponse.next();
+
+
+  if (req.method !== "OPTIONS") {
+    response.headers.set("Access-Control-Allow-Origin", "*");
+    response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  }
+
+  return response;
+}
+
+
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/','/api/webhook','/api/subscribe']);
 
 export default clerkMiddleware((auth, request) => {
   if(!isPublicRoute(request)) {
